39250cecdcc2e5fd55bc8ad10f6e6858bf375613
slavabily <slavabily@me.com>
2020-01-29 21:46:08 +0200

    Initial Commit

---
+++ Guess_animal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess_animal
+//
+//  Created by slava bily on 29/1/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
